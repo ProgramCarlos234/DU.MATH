a59a72f2c9514900a160d01f6a636378bc7e9fa8
ProgramCarlos234 <74023635@continental.edu.pe>
2025-08-10 19:38:48 -0500

    Actualizacion_Carlos_10_25

--- du.math_demo/Scripts/game_manager.gd
+++ du.math_demo/Scripts/game_manager.gd
@@ -21,13 +21,17 @@ const ENTORNO_RPG = preload("res://Scenas/ScenasEntorno/EntornoRPG.tscn")
 const ENTORNO_PLATAFORMAS = preload("res://Scenas/ScenasEntorno/EntornoPlataformas.tscn")
 const NIVEL_1_MOVER_CAJAS = preload("res://Scenas/ScenasEntorno/Nivel1_MoverCajas.tscn")
 const MAPA_JUEGO_ISLAND = preload("res://Scenas/ScenasEntorno/Mapa_juego_Island.tscn")
+const SUB_NIVELES = preload("res://Scenas/ScenasEntorno/sub_niveles.tscn")
+const NIVEL_1_2_DIVISORES = preload("res://Scenas/ScenasEntorno/Nivel1_2Divisores.tscn")
 
 var Scenas: Array = [
 	MAPA_JUEGO,
 	ENTORNO_RPG,
 	ENTORNO_PLATAFORMAS,
 	NIVEL_1_MOVER_CAJAS,
-	MAPA_JUEGO_ISLAND
+	MAPA_JUEGO_ISLAND,
+	SUB_NIVELES,
+	NIVEL_1_2_DIVISORES
 ]
 
 # ======================= #
--- du.math_demo/Scripts/portal.gd
+++ du.math_demo/Scripts/portal.gd
@@ -3,7 +3,18 @@ extends Area2D
 @export var Indicador_mundo: int
 @export var TipoDePortal: bool
 
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+@onready var sprite_2d_2: AnimatedSprite2D = $Sprite2D2
 
+func _ready() -> void:
+	if TipoDePortal:
+		sprite_2d.visible = true
+		sprite_2d_2.visible = false
+	else:
+		sprite_2d_2.visible = true
+		sprite_2d.visible = false
+	pass
+	
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Jugador": # Verifica que el cuerpo que entró sea el jugador
 		body.valor = Indicador_mundo # Le pasa el valor del indicador al jugador
